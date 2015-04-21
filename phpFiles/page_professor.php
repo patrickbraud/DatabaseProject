8c9550f6e3b77c4acef49f3c57a253ed1f2162a1
Patrick <patrick.braud@ttu.edu>
2015-04-21 17:24:02 -0500

    Please Fix my Files

---
+++ phpFiles/page_professor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			ul {
+    			list-style-type: none;
+    			margin: 1;
+    			padding: 0;
+			}
+
+			li {
+    			display: inline;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<ul>
+  			<li><a href="page_faculty_staff.php">Faculty/Staff</a></li>
+  			<li><a href="page_professor.php">Professor</a></li>
+  			<li><a href="page_business_manager.php">Business Manager</a></li>
+  			<br><br><br>
+			<li><a href="insert_preferences.php">Insert Preferences</a></li>
+			<br><br>
+			<li><a href="insert_load_distr.php">Load Distribution</a></li>
+			<br><br>
+			<li><a href="insert_special_req.php">Special Requests</a></li>
+			<br><br>
+			<li><a href="insert_book_info.php">Insert Text Book</a></li>
+			<br><br>
+			<li><a href="view_next_semester.php">Next Semester Courses</a></li>
+			<br><br>
+		</ul>
+	</body>
+</html>
