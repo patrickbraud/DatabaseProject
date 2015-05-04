367a9d514b7f8a943739b6a8343347501c098119
rydaily <ryan.daily@ttu.edu>
2015-05-03 19:51:46 -0500

    Search Instructor and Pref work

--- phpFiles/insert_preferences.php
+++ phpFiles/insert_preferences.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <html>
 <head>
 	<style>
@@ -19,14 +23,39 @@
   	<li><a href="page_professor.php">Back</a></li>
 	<br>
 	<?php
-    echo "<br>New Instructor Information<br><br>";
+    echo "<br>Instructor Preferences<br><br>";
 	?>
 </ul>
 
-<form name='Input Professor Preferences' action='insert_professor_preference.php' method='post'>
-<br><br>
-<input type="submit" name="submit" value="Submit">
-</form>
+<?php
+if(isset($_POST['update']))
+{
 
+$_SESSION["name"] = "Lim";
+
+}
+else
+{
+?>
+
+<form method="post" action="prefs.php">
+	<table width="400" border="0" cellspacing="1" cellpadding="2">
+		<tr>
+			<td width="300">Select a Professor to Update</td>
+			<td><input name="name" type="text" id="name"></td>
+		</tr>
+		<tr>
+			<td width="100"> </td>
+			<td> </td>
+		</tr>
+		<tr>
+			<td width="100"> </td>
+			<td><input name="update" type="submit" id="update" value="Submit"></td>
+		</tr>
+	</table>
+</form>
+<?php
+}
+?>
 </body>
 </html>
--- phpFiles/search_instructor_course_info.php
+++ phpFiles/search_instructor_course_info.php
@@ -43,12 +43,22 @@ if($year < 0){
 	$year = $year + 100;
 }
 
+mysql_select_db('projectdb');
+
 $sql1 = "SELECT code, title, courseSectionLink.semester, courseSectionLink.year, enrollment, info.room_num " .
 		"FROM courseSectionLink, info " . 
-		"WHERE courseSectionLink.year >= '$year' AND instr_name = '$name' AND courseSectionLink.id = info.id; ";
-	   
-mysql_select_db('projectdb');
+		"WHERE courseSectionLink.year >= '$year' AND instr_name = '$name' AND courseSectionLink.id = info.id " .
+		"Order By year DESC; ";
 $retval = mysql_query( $sql1, $conn );
+
+$sql2 = "Select courseSectionLink.code as cc, count(courseSectionLink.code) As times, avg(ta_hours) As avg_ta, avg(enrollment) As avg_enroll, isReq " . 
+		"From courseSectionLink, assign, course " .
+		"Where courseSectionLink.instr_name = '$name' AND courseSectionLink.year >= '$year' AND assign.crn = courseSectionLink.crn AND course.code = courseSectionLink.code " . 
+		"Group By courseSectionLink.code " .
+		"Order By courseSectionLink.code ASC; ";
+
+$retval2 = mysql_query( $sql2, $conn );
+$retval3 = mysql_query( $sql2, $conn );
 if(! $retval)
 {
   die('Could not update data: ' . mysql_error());
@@ -60,18 +70,79 @@ if(mysql_num_rows($retval) > 0) {
     
     while($row = mysql_fetch_array($retval)) {
         echo "<tr>
-                <td> - Code: " . $row["code"]. "</td>
-                <td> - Title: " . $row["title"]. "</td>
+                <td> - Code: " . $row["code"]. "</td>            
+				<td> - Title: " . $row["title"]. "</td>
                 <td> - Semester: " . $row["semester"]. " " . $row["year"]. "</td>
                 <td> - Enrollment: " . $row["enrollment"]. "</td>
                 <td> - Building: " . $row["room_num"]. "</td>
-            </tr>" ;
+			</tr>" ;
     }
     echo "</table>";
 } else {
     echo "<font color = 'red' >0 results\n";
 }
-    
+
+if(! $retval2)
+{
+  die('Could not update data: ' . mysql_error());
+}
+echo "<br> Undergraduate Courses <br><br>";
+if(mysql_num_rows($retval2) > 0) {
+    // output data of each row
+    echo "<table id = 't01' style = 'width:50%'><caption></ caption>";
+    while($row = mysql_fetch_array($retval2)) {
+		if($row["avg_ta"] == 0){
+			$ratio = "N/A";
+		}
+		else{
+			$ratio = $row["avg_ta"]/$row["avg_enroll"];
+		}
+		if($row["cc"][2] < 5){
+        echo "<tr>
+				<td> - Course: " . $row["cc"]. "</td>
+				<td> - Times Offered: " . $row["times"]. "</td>
+				<td> - TA Hours: " . $row["avg_ta"] . "</td>
+				<td> - Enrollment: " . $row["avg_enroll"]. "</td>
+				<td> - Ta Hours/Students Enrolled: " . $ratio . "</td>
+				<td> - Required: " . $row["isReq"] . "</td>
+            </tr>" ;
+		}
+    }
+    echo "</table>";
+} else {
+    echo "<font color = 'red' >0 results\n";
+}
+if(! $retval3)
+{
+  die('Could not update data: ' . mysql_error());
+}
+echo "<br> Graduate Courses <br><br>";
+if(mysql_num_rows($retval3) > 0) {
+    // output data of each row
+    echo "<table id = 't01' style = 'width:50%'><caption></ caption>";
+    while($row = mysql_fetch_array($retval3)) {
+		if($row["avg_ta"] == 0){
+			$ratio = "N/A";
+		}
+		else{
+			$ratio = $row["avg_ta"]/$row["avg_enroll"];
+		}
+		if($row["cc"][2] == 5){
+        echo "<tr>
+				<td> - Course: " . $row["cc"]. "</td>
+				<td> - Times Offered: " . $row["times"]. "</td>
+				<td> - TA Hours: " . $row["avg_ta"] . "</td>
+				<td> - Enrollment: " . $row["avg_enroll"]. "</td>
+				<td> - Ta Hours/Students Enrolled: " . $ratio . "</td>
+				<td> - Required: " . $row["isReq"] . "</td>
+            </tr>" ;
+		}
+    }
+    echo "</table>";
+} else {
+    echo "<font color = 'red' >0 results\n";
+}
+
 mysql_close($conn);
 }
 else
